e8182578908162a6e1f8b17eb74d0b6d47a1af59
RamiroTelles <78386364+RamiroTelles@users.noreply.github.com>
2023-09-21 22:07:21 -0600

    leer nombres

--- Practica 3/prueba.asm
+++ Practica 3/prueba.asm
@@ -22,6 +22,18 @@ mensaje_cargar db "Cargar Partida",0a,"$"
 mensaje_ayuda db "Ayuda",0a,"$"
 mensaje_salir db "Saliendo",0a,"$"
 
+mensaje_nombre_1 db "Escriba el nombre del jugador 1: $"
+mensaje_nombre_2 db "Escriba el nombre del jugador 2: $"
+
+
+
+nombre1         db 00
+                db 20 dup (00)
+nombre2         db 00
+                db 20 dup (00)
+buffer_nombre   db 20,00
+                db 20 dup (00)
+
 
 .CODE 
 .STARTUP 
@@ -83,9 +95,9 @@ menu:
         int 21
         ;;
         cmp AL,'1'
-        je PvP
+        je ingresar_2nombres
         cmp AL,'2'
-        je PvC
+        je ingresar_1nombre
         cmp AL,'3'
         je cargar_partida
         cmp AL,'4'
@@ -93,12 +105,64 @@ menu:
         cmp AL,'5'
         je final
         jmp menu
+ingresar_2nombres:
+        mov DX, offset mensaje_nombre_1
+	mov AH, 09
+	int 21
+        mov DX, offset buffer_nombre
+	mov AH, 0a
+	int 21
+        ;;
+        mov DI, offset nombre1
+        call copiar_cadena
+        ;;
+        mov DX, offset saltoLinea
+        mov AH,09
+        int 21
+        ;;
+        mov DX, offset mensaje_nombre_2
+	mov AH, 09
+	int 21
+        mov DX, offset buffer_nombre
+	mov AH, 0a
+	int 21
+        ;;
+        mov DI, offset nombre2
+        call copiar_cadena
+        jmp PvP
+ingresar_1nombre:
+        mov DX, offset mensaje_nombre_1
+	mov AH, 09
+	int 21
+        ;;
+        mov DX, offset buffer_nombre
+	mov AH, 0a
+	int 21
+        ;;
+        mov DI, offset nombre1
+        call copiar_cadena
+        ;;
+        mov DI, offset nombre2
+        mov AL, 03
+        mov [DI],AL
+        inc DI
+        mov AL,'C'
+        mov [DI],AL
+        inc DI
+        mov AL, 'P'
+        mov [DI],AL
+        inc DI
+        mov AL, 'U'
+        mov [DI],AL
+        jmp PvC
 PvP:
+        call imprimir_jugadores
         mov DX, offset mensaje_pvp
         mov AH,09
         int 21
         jmp menu
 PvC:
+        call imprimir_jugadores
         mov DX, offset mensaje_pvC
         mov AH,09
         int 21
@@ -114,8 +178,57 @@ ayuda:
         mov AH,09
         int 21
         jmp menu
+copiar_cadena:
+        mov SI, offset buffer_nombre
+        inc SI
+        mov AL, [SI]
+        mov [DI],AL
+        inc SI
+        inc DI
 
-
+        mov CX,0000
+        mov CL,AL
+ciclo_copiarCadena:
+        mov AL,[SI]
+        mov [DI],AL
+        inc SI
+        inc DI
+        loop ciclo_copiarCadena
+        ret
+imprimir_jugadores:
+        ;;; imprimir el primer nombre
+	;
+	;
+        mov DX, offset saltoLinea
+	mov AH, 09
+	int 21
+        ;
+	mov BX, 0001
+	mov CX, 0000
+	mov CL, [nombre1]
+        mov DI, offset nombre1
+	inc DI
+	mov DX, DI
+	mov AH, 40
+	int 21
+        ;
+	mov DX, offset saltoLinea
+	mov AH, 09
+	int 21
+	;;; imprimir el segundo nombre
+	mov BX, 0001
+	mov CX, 0000
+	mov CL, [nombre2]
+	mov DI, offset nombre2
+	inc DI
+	mov DX, DI
+	mov AH, 40
+	int 21
+	mov DX, offset saltoLinea
+	mov AH, 09
+	int 21
+        ;
+        ret
 
 
 final:
